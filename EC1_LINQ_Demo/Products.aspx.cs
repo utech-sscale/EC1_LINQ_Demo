51f78793c76b7358bb806d244a658e40f118e4fa
Stephen Scale <sscale@utech.edu.jm>
2019-10-08 16:34:42 -0500

    Add project files.

---
+++ EC1_LINQ_Demo/Products.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EC1_LINQ_Demo
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dataContext = new DataClasses1DataContext();
+
+            GridView1.DataSource = from product in dataContext.Products
+                                   select new
+                                   {
+                                       product.productName,
+                                       product.productDescription,
+                                       product.unitPrice
+                                   };
+
+            GridView1.DataBind();
+        }
+    }
+}
